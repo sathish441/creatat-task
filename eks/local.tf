94c717c8788eb97b06099480dde4e26aca549f4c
Mallesh <malleshdevops2021@gmail.com>
2022-09-14 19:59:05 +0530

    Added eks terraform code

---
+++ eks/local.tf
@@ -0,0 +1,18 @@
+locals {
+worker_groups = [
+    {
+      name                          = "worker-group-1"
+      instance_type                 = "t2.small"
+      additional_userdata           = "echo foo bar"
+      additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
+      asg_desired_capacity          = 2
+    },
+    {
+      name                          = "worker-group-2"
+      instance_type                 = "t2.medium"
+      additional_userdata           = "echo foo bar"
+      additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
+      asg_desired_capacity          = 1
+    },
+  ]
+}
