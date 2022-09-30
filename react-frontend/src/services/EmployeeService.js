60fc76d286442811acfc0e9a5057e9682f0b0220
mallesh <malleshdevops2021@gmail.com>
2022-09-30 11:30:33 +0000

    added react js applicaiton

---
+++ react-frontend/src/services/EmployeeService.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+const EMPLOYEE_API_BASE_URL = "http://a980054ef8ec44900a0687b51f37717d-344732415.us-east-2.elb.amazonaws.com:8080/api/v1/employees";
+
+class EmployeeService {
+
+    getEmployees(){
+        return axios.get(EMPLOYEE_API_BASE_URL);
+    }
+
+    createEmployee(employee){
+        return axios.post(EMPLOYEE_API_BASE_URL, employee);
+    }
+
+    getEmployeeById(employeeId){
+        return axios.get(EMPLOYEE_API_BASE_URL + '/' + employeeId);
+    }
+
+    updateEmployee(employee, employeeId){
+        return axios.put(EMPLOYEE_API_BASE_URL + '/' + employeeId, employee);
+    }
+
+    deleteEmployee(employeeId){
+        return axios.delete(EMPLOYEE_API_BASE_URL + '/' + employeeId);
+    }
+}
+
+export default new EmployeeService()
